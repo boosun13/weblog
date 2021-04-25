2da085756e68e31bad4303c5d435d253701f0454
boosun13 <double0two22@gmail.com>
2021-04-25 19:30:49 +0900

    データベースの準備

---
+++ config/mongodb.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  CONNECTION_URL: "mongodb://user:user@localhost:27017/",
+  DATABASE: "weblog",
+  OPTIONS: {
+    family: 4,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+};
