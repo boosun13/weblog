69bec2847ed9a5057cd07b3316acd9b160e3c794
boosun13 <double0two22@gmail.com>
2021-04-30 01:13:32 +0900

    記事登録の確認画面追加

--- routes/account.js
+++ routes/account.js
@@ -1,15 +1,35 @@
 var router = require("express").Router();
 
+var validateRegistData = function (body) {
+  var isValidated = true, errors = {};
+
+  if (!body.url) {
+    isValidated = false;
+    errors.url = "URLが未入力です。'/'から始まるURLを入力してください。";
+  }
+
+  if (body.url && /^\//.test(body.url) === false) {
+    isValidated = false;
+    errors.url = "'/'から始まるURLを入力してください。";
+  }
+
+  if (!body.title) {
+    isValidated = false;
+    errors.title = "タイトルが未入力です。任意のタイトルを入力してください。";
+  }
+
+  return isValidated ? undefined : errors;
+};
 var createRegistData = function (body) {
   var datetime = new Date();
   return {
     url: body.url,
     published: datetime,
-    update: datetime,
+    updated: datetime,
     title: body.title,
     content: body.content,
     keywords: (body.keywords || "").split(","),
-    authors: (body.keywords || "").split(","),
+    authors: (body.authors || "").split(","),
   };
 };
 
@@ -26,4 +46,13 @@ router.post("/posts/regist/input", (req, res) => {
   res.render("./account/posts/regist-form.ejs", { original });
 });
 
+router.post("/posts/regist/confirm", (req, res) => {
+  var original = createRegistData(req.body);
+  var errors = validateRegistData(req.body);
+  if (errors) {
+    res.render("./account/posts/regist-form.ejs", { errors, original });
+    return;
+  }
+  res.render("./account/posts/regist-confirm.ejs", { original });
+});
 module.exports = router;
