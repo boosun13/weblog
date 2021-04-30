a2cc5e315d0d54fba7fc1aa7f60766cda9b6e4e1
boosun13 <double0two22@gmail.com>
2021-05-01 00:23:06 +0900

    記事登録機能の完了画面作成

--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,6 @@
+var { CONNECTION_URL, OPTIONS, DATABASE } = require("../config/mongodb.config");
 var router = require("express").Router();
+var MongoClient = require("mongodb").MongoClient;
 
 var validateRegistData = function (body) {
   var isValidated = true, errors = {};
@@ -55,4 +57,27 @@ router.post("/posts/regist/confirm", (req, res) => {
   }
   res.render("./account/posts/regist-confirm.ejs", { original });
 });
+
+router.post("/posts/regist/execute", (req, res) => {
+  var original = createRegistData(req.body);
+  var errors = validateRegistData(req.body);
+  if (errors) {
+    res.render("./account/posts/regist-form.ejs", { errors, original });
+    return;
+  }
+
+  MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
+    var db = client.db(DATABASE);
+    db.collection("posts")
+      .insertOne(original)
+      .then(() => {
+        res.render("./account/posts/regist-complete.ejs");
+      }).catch((error) => {
+        throw error;
+      }).then(() => {
+        client.close();
+      });
+  });
+});
+
 module.exports = router;
